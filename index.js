10bf2cf409afcc11cdd112d994680949ba7bdcf6
Diego Cunha <diegocunha.bsi@gmail.com>
2019-08-20 13:21:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Homepage! Hello world.');
+});
+
+app.get('/about', (req, res) => { // new
+  res.send('About page. Nice.');
+});
+
+app.listen(3000, () => console.log('listening on port 3000'));
