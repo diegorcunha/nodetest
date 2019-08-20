ade512447359a920fd5a3c3356618c017d967fc5
Diego Cunha <diegocunha.bsi@gmail.com>
2019-08-20 14:18:23 -0300

    Test 02

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Homepage! Hello world Bitchhhh.');
+  res.send('Homepage! Hello world.');
 });
 
 app.get('/about', (req, res) => { // new
