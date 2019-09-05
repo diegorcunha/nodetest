6eeb4aac3403920b4b68eb3418cfc68e2d00b2da
Diego Rogério da Cunha <39497916+diegorcunha@users.noreply.github.com>
2019-09-05 11:53:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Homepage! Hello world.');
+  res.send('Homepage! Hello world. Midhaz pra sempre!!!!');
 });
 
 app.get('/about', (req, res) => { // new
